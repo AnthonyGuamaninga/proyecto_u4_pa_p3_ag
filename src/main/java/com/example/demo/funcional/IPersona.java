71b84f13350b1dbff7b14f89392e94d622a87e72
Anthony Guamaninga <anthonysub300@gmail.com>
2023-02-24 19:45:47 -0500

    Taller 32: Paradigmas y tipos de lambdas

---
+++ src/main/java/com/example/demo/funcional/IPersona.java
@@ -0,0 +1,6 @@
+package com.example.demo.funcional;
+
+@FunctionalInterface // no es necesaria la anotacioon, se utiliza la anotacion como buena practica 
+public interface IPersona {
+	public String comer();
+}
